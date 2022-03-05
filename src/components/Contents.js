47ed91958d22e524946b5067533315cdf123c743
donnypm <donnypramudia04@gmail.com>
2022-03-05 20:02:39 +0700

    first commit

---
+++ src/components/Contents.js
@@ -0,0 +1,28 @@
+import "./Contents.scss";
+import Data from "../data/content.json";
+
+const Contents = () => {
+  return (
+    <div>
+      <div>
+        <h1 style={{ textAlign: "center", marginTop: "0px" }}>Information</h1>
+        <div>
+          {Data.data.map((content) => {
+            return (
+              <div key={content.id} style={{ marginBottom: "20px" }}>
+                <img src={content.image} className="content-image" alt="" />
+                <h3>{content.title}</h3>
+                <p className="content-paragraf">{content.content}</p>
+                <a href={content.link} className="content-link">
+                  More Info
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contents;
